41b9b73427c1ea2be0a2561e19c4639803cc78b0
Brittny Rosenow <brittnyrosenow@gmail.com>
2020-06-28 16:59:49 -0600

    udpated length

--- script.js
+++ script.js
@@ -19,10 +19,7 @@ function generate() {
 
     // add password to previousl gen password section
     document.getElementById("lastNums").innerHTML += password + "<br />";
-
+}
     // site default length display of 25 
     document.getElementById("length").innerHTML = "length:25";
 
-    
-
-}
