34d43db84d2d8debda171d312b060bc4141aea13
Brittny Rosenow <brittnyrosenow@gmail.com>
2020-06-28 16:01:48 -0600

    updated srcipt.s with password

---
+++ scritp.js
@@ -0,0 +1,18 @@
+// generate random password
+
+function generate() {
+
+    //set password lenth/complexity
+    let complexity = document.getElementById("slider").value;
+    // pasword values
+    let values = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567980!@#$%^&*()_+";
+
+    let password = "";
+
+    // create 4 loo to choose password char
+
+    for(var i = 0; i <= complexity; i++){
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - ))))
+    }
+
+}
